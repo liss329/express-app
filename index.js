7af98a2d3f5d327006db506487a6057dca1ddf62
Lissana <liss.glace471@gmail.com>
2022-04-20 07:25:09 +0900

    feat: テンプレートエンジンの設定

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const ejs = require('ejs');
+
 const app = express();
+app.engine('ejs', ejs.renderFile);
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Welcom to Express');
+    // index.ejsをレンダリングする
+    res.render('index.ejs', {
+        title: 'Index',
+        content: 'This is Express-app Top page',
+    });
 });
 
 app.listen(3000, () => {
