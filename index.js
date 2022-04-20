88144b4bb635269befdcdb3c769b7a6d1d2d79a0
Lissana <liss.glace471@gmail.com>
2022-04-20 12:16:04 +0900

    feat: フォーム処理の追加

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const express = require('express');
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.engine('ejs', ejs.renderFile);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     const msg =
-        `This is Express-app Top page<br>
-        これはトップページです`;
+        `This is Index page<br>
+        ※メッセージを書いて送信してください。`;
     res.render('index.ejs', {
         title: 'Index',
         content: msg,
-        link: { href: '/other', text: '※別のページに移動' },
+    });
+});
+
+app.post('/', (req, res) => {
+    const msg =
+        `This is Posted page<br>
+        あなたは${req.body.message}と送信しました。`;
+    res.render('index.ejs', {
+        title: 'Posted',
+        content: msg
     });
 });
 
 app.get('/other', (req, res) => {
+    const name = req.query.name;
+    const pass = req.query.pass;
     const msg =
-        `This is Express-app Top page<br>
-        これは用意された別のページです`;
+        `あなたの名前は${name}<br>
+        パスワードは${pass}です。`;
     res.render('index.ejs', {
         title: 'other',
         content: msg,
